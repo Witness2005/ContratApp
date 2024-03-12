282498fa5f2d4f6a32a1d9253fc406a963aa3c9d
JuanPa <jupablota2005@gmail.com>
2024-03-11 20:37:17 -0500

    Servidor Base

--- src/DataBase/connection.js
+++ src/DataBase/connection.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 var dataBase = 'contratAppDB';
 var userName = 'postgres';
-var password = 'tomas18';
+var password = '*****';
 
 const connection = new Sequelize(dataBase, userName, password,{
     host: 'localhost',
